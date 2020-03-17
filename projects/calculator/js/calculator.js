78c4d411ea17a2d55d374ae47b8f9662a196fa0b
Nell Himlie <33374005+himlne01@users.noreply.github.com>
2020-03-17 03:00:11 -0500

    THE FINAL

--- projects/calculator/js/calculator.js
+++ projects/calculator/js/calculator.js
@@ -1,73 +1,41 @@
-/* jshint esversion: 6 */
+/* jshint esversion: 8 */
 /* jshint node: true */
 /* jshint browser: true */
 /* jshint jquery: true */
-'use strict';
+"use strict";
 
-class theCalcStack {
-    constructor(stack) {
-        this._stack = stack;
-    }
+let str = "";
+let total = "";
 
-    get stack() {
-        return this._stack;
-    }
-
-    clearCalculator() {
-        for (var things in this._stack) {
-            this._stack.pop();
-        }
-        this._stack.push(0);
-        this._stack.get();
-    }
-    
-    onStack(thing) {
-        this._stack.push(thing);
-    }
-    
-    onStackAdd() {
-    	evaluate();
-        this._stack.push("+");
-    }
-    
-    onStackSubtract() {
-    	evaluate();
-        this._stack.push("-");
-        
-    }
-    
-    onStackMultiply() {
-        evaluate();
-        this._stack.push("*");
-    }
-    
-    onStackDivide() {
-    	evaluate();
-        this._stack.push("/");
-    }
-    
-    onStackDec() {
-    	evaluate();
-        this._stack.push(".");
-    }
-    
-    evaluate() {
-    	let numberOne="";
-    	let numberTwo="";
-    	let opFocus="";
-        for (var n in this._stack){
-        	n = this._stack.pop();
-            if (typeof(n) === "number") {
-                numberOne.concat(n);
-            } else if (typeof(n) !== "number"){
-				opFocus = n
-				numberTwo = numberOne;
-				numberOne = ""
-				
-            }
-        return numberOne opFocus numberTwo;
-        }
-    
-    }
+function onStack(given) {
+	if (total == '') {
+		clears();
+	}
+	total = total + given;
+	let ansBox = document.querySelector(".para");
+	ansBox.append(given);
 }
 
+function clears() {
+	let ansRow = document.querySelector("#theans");
+	let ansBox = document.querySelector(".para");
+	ansRow.removeChild(ansBox);
+	let p = document.createElement("p");
+	let textnode = document.createTextNode(' ');
+	p.appendChild(textnode);
+	p.classList = "para";
+	ansRow.appendChild(p);
+}
+
+function clearsOne() {
+	clears();
+	total = "";
+}
+
+function evaluated() {
+	let ansRow = document.querySelector(".para");
+	clears();
+	ansRow = document.querySelector(".para");
+	ansRow.append(eval(total));
+	total = '';
+}
