57a86841306e52441c72c87057bffac5724bd335
himlne01 <himlne01@luther.edu>
2020-04-29 17:47:50 -0500

    Heroku databasegit add .

--- projects/store/app.py
+++ projects/store/app.py
@@ -1,16 +1,16 @@
 import psycopg2
 from flask import Flask, make_response, redirect, render_template, request, url_for
 
-def create_app():
-    app = Flask(__name__)
-    return app
+# def create_app():
+app = Flask(__name__)
+    # return app
 
 def get_data_from_db(query: str) -> list:
     conn = psycopg2.connect(
-        database = "daqj1d7gm0fvcn",
-        user="mdtbclhttkczxo",
-        password="ed7f7afc595ede2cea13630c0ef7f1082346763235e31f5f5cf3ff9ae9085b94",
-        host="ec2-34-233-186-251.compute-1.amazonaws.com",
+        database = "dqq9otsot3o58",
+        user="snpszposoqyjmu",
+        password="61198af4d3436cef0a226f1e76a3ac3dc6dac565f244d0df4df595ba0183160f",
+        host="ec2-54-159-112-44.compute-1.amazonaws.com",
         port="5432"
         # database = "inventory",
         # user="postgres",
@@ -20,9 +20,11 @@ def get_data_from_db(query: str) -> list:
     )
     cur = conn.cursor()
     cur.execute(query)
+
     if query[0] == 'I':
         conn.commit()
-        display = 'SELECT * FROM public."inventoryTable";'
+        display = 'SELECT * FROM users;'
+        # display = 'SELECT * FROM public."inventoryTable";'
         cur.execute(display)
     rows = cur.fetchall()
     return rows
@@ -34,7 +36,8 @@ def index():
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "GET":
-        query = 'SELECT * FROM public."inventoryTable";'
+        query = 'SELECT * FROM users;'
+        # query = 'SELECT * FROM public."inventoryTable";'
         db=get_data_from_db(query)
         return render_template("add.html", iceCream=db)
     else:
@@ -43,14 +46,16 @@ def add():
         price = request.form.get("price")
         quantity = request.form.get("quantity")
 
-        query = "INSERT INTO public.\"inventoryTable\" (name, category, price, quantity) VALUES ('" + name + "', '" + category + "', '" + price + "', '" + quantity + "');"
+        query = "INSERT INTO users (name, category, price, quantity) VALUES ('" + name + "', '" + category + "', '" + price + "', '" + quantity + "');"
+        # query = "INSERT INTO public.\"inventoryTable\" (name, category, price, quantity) VALUES ('" + name + "', '" + category + "', '" + price + "', '" + quantity + "');"
         db=get_data_from_db(query)
 
         return render_template("add.html", iceCream=db)
 
 @app.route("/list")
 def list():
-    query = 'SELECT * FROM public."inventoryTable";'
+    query = 'SELECT * FROM users;'
+    # query = 'SELECT * FROM public."inventoryTable";'
     db=get_data_from_db(query)
     return render_template("list.html", iceCream=db)
 
