406a20d1a15bcf8001f684916e4b9355237b2c49
himlne01 <himlne01@luther.edu>
2020-05-03 21:57:02 -0500

    Ex login

--- exercises/login/.flaskenv
+++ exercises/login/.flaskenv
@@ -1,2 +1,3 @@
+FLASK_APP=app.py
 FLASK_ENV=development
 SECRET_KEY=b'_5#y2L"F4Q8z\n\xec]/'
--- exercises/login/app.py
+++ exercises/login/app.py
@@ -8,14 +8,22 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 @app.route("/")
 def index():
-    raise NotImplementedError
+    if "username" in session:
+        return render_template("index.html", inUse = session["username"])
+    return redirect(url_for("login"))
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    raise NotImplementedError
+    if request.method == "GET":
+        return render_template("login.html")
+    else: 
+        name = request.form.get("username")
+        session["username"] = request.form["username"]
+        return redirect(url_for("index"))
 
 
 @app.route("/logout", methods=["POST"])
 def logout():
-    raise NotImplementedError
+    session.pop("username", None)
+    return redirect(url_for("login"))
